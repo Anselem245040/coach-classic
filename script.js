eabef600b1502c4e51b1acebf87e4647d1265fe5
paulDev <paulanselem35@gmail.com>
2025-03-13 21:30:48 +0100

    removed unwanted lines of code

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var form = document.getElementById("my-form");
+let form = document.getElementById("my-form");
 async function handleSubmit(event) {
   event.preventDefault();
-  var status = document.getElementById("my-form-status");
-  var data = new FormData(event.target);
+  let status = document.getElementById("my-form-status");
+  let data = new FormData(event.target);
   fetch(event.target.action, {
     method: form.method,
     body: data,
